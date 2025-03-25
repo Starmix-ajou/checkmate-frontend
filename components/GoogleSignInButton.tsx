373c6f12dced06b96f4995fb879ef0568cd7548a
pjookim <pjookim@naver.com>
2025-03-25 12:06:00 +0900

    feat: next-auth를 통한 로그인 핸들링 (CM-12)

--- components/GoogleSignInButton.tsx
+++ components/GoogleSignInButton.tsx
@@ -1,13 +1,18 @@
 'use client'
 
+import { signIn } from 'next-auth/react'
 import { Button } from '@/components/ui/button'
 
 export default function GoogleSignInButton() {
+  const handleGoogleSignIn = () => {
+    signIn('google')
+  }
   return (
     <Button
       variant="outline"
       size="lg"
       className="w-full flex items-center justify-center gap-2 bg-blue-500 text-white hover:bg-blue-600 focus:ring-2 focus:ring-blue-500"
+      onClick={handleGoogleSignIn}
     >
       Sign in with Google
     </Button>
