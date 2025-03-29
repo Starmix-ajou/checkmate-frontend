033d8341b46f806e7f14068e2b736b69512c9479
pjookim <pjookim@naver.com>
2025-03-29 15:17:56 +0900

    fix: 팀원 수에 따라 Avatar가 제대로 표시되지 않는 문제 해결 (CM-14)

--- components/project/overview/AvatarGroup.tsx
+++ components/project/overview/AvatarGroup.tsx
@@ -8,17 +8,21 @@ interface AvatarGroupProps {
 }
 
 export default function AvatarGroup({ users, className }: AvatarGroupProps) {
+  const displayedUsers = users.slice(0, 4)
+  const remainingUsers = users.length - displayedUsers.length
   return (
     <div className={`flex -space-x-2 ${className}`}>
-      {users.map((user, index) => (
+      {displayedUsers.map((user, index) => (
         <Avatar key={index}>
           <AvatarImage src={user.src} />
           <AvatarFallback>{user.name.charAt(0)}</AvatarFallback>
         </Avatar>
       ))}
-      <Avatar className="border border-white bg-gray-200">
-        <AvatarFallback>+{users.length - 3}</AvatarFallback>
-      </Avatar>
+      {remainingUsers > 0 && (
+        <Avatar className="border border-white bg-gray-200">
+          <AvatarFallback>+{remainingUsers}</AvatarFallback>
+        </Avatar>
+      )}
     </div>
   )
 }
