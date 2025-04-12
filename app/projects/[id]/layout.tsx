dc52926a73ef822d412d8b0a0d63cbe6f1ad5d94
wsd9489@ajou.ac.kr <wsd9489@ajou.ac.kr>
2025-04-12 16:20:16 +0900

    refactor: sidebar trigger와 children을 MainContent로 분리 (CM-33)

--- app/projects/[id]/layout.tsx
+++ app/projects/[id]/layout.tsx
@@ -1,10 +1,11 @@
 import ProjectSidebar from '@/components/project/Sidebar'
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { SidebarProvider } from '@/components/ui/sidebar'
 import '@blocknote/core/fonts/inter.css'
 import '@blocknote/mantine/style.css'
 import '@liveblocks/react-ui/styles.css'
 import '@liveblocks/react-ui/styles/dark/media-query.css'
 import '@liveblocks/react-tiptap/styles.css'
+import MainContent from '@/components/MainContent'
 
 export default function ProjectsLayout({
   children,
@@ -14,10 +15,7 @@ export default function ProjectsLayout({
   return (
     <SidebarProvider>
       <ProjectSidebar />
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
+      <MainContent>{children}</MainContent>
     </SidebarProvider>
   )
 }
