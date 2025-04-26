1ad99deec642ebe1d21ece6714c842fed4bcbf29
wsd9489@ajou.ac.kr <wsd9489@ajou.ac.kr>
2025-04-26 15:58:24 +0900

    style: Prettier 포맷팅 수정 (CM-52)

--- components/project/new/phases.ts
+++ components/project/new/phases.ts
@@ -6,21 +6,21 @@ export const phases: Phase[] = [
     title: '프로젝트 생성',
     question: '어떤 프로젝트를\n기획했나요?',
     type: 'form',
-    inputType: 'text'
+    inputType: 'text',
   },
   {
     id: 2,
     title: '프로젝트 이름',
     question: '좋아요! 프로젝트 이름이 정해졌나요?',
     type: 'chat',
-    inputType: 'text'
+    inputType: 'text',
   },
   {
     id: 3,
     title: '프로젝트 기간 설정',
     question: '멋진 이름이네요! 예상 기발 기간을 알려주세요.',
     type: 'chat',
-    inputType: 'dateRange'
+    inputType: 'dateRange',
   },
   {
     id: 4,
@@ -28,13 +28,13 @@ export const phases: Phase[] = [
     question:
       '이제 팀원을 초대할 차례예요. 프로젝트에 참여할 팀원은 총 몇 명인가요?',
     type: 'chat',
-    inputType: 'number'
+    inputType: 'number',
   },
   {
     id: 5,
     title: '프로젝트 기능 정의서',
     question: '기능 정의서가 준비되어 있나요?',
     type: 'chat',
-    inputType: 'file'
+    inputType: 'file',
   },
 ]
