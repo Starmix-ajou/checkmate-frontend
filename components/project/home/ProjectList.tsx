0dedcd7dda0e6ff5361df6eaa5ce34a52346cb85
pjookim <pjookim@naver.com>
2025-04-27 21:01:26 +0900

    feat: Server로부터 프로젝트 목록 fetch (CM-60)

--- components/project/home/ProjectList.tsx
+++ components/project/home/ProjectList.tsx
@@ -1,29 +1,44 @@
 // components/project/home/ProjectList.tsx
 import ProjectCard from '@/components/project/home/ProjectCard'
 
-type ProjectListProps = {
-  filter: string
-  projects: {
-    id: number
-    name: string
-    status: string
-    startDate: string
-    endDate: string
-  }[]
+type Profile = {
+  stacks: string[]
+  positions: string[]
+  projectId: string
 }
 
-const ProjectList = ({ filter, projects }: ProjectListProps) => {
-  const filteredProjects = projects.filter(
-    (project) => filter === 'all' || project.status === filter
-  )
+type Member = {
+  name: string
+  email: string
+  profileImageUrl: string
+  profiles: Profile[]
+  role: string
+  pendingProjectIds: string[]
+}
 
+type Project = {
+  projectId: string
+  projectTitle: string
+  projectImageUrl: string
+  profile: Profile
+  startDate: string
+  endDate: string
+  members: Member[]
+  leader: Member
+}
+
+type ProjectListProps = {
+  projects: Project[]
+}
+
+const ProjectList = ({ projects }: ProjectListProps) => {
   return (
-    <div className="flex justify-around gap-6">
-      {filteredProjects.map((project) => (
+    <div className="flex justify-around gap-6 flex-wrap">
+      {projects.map((project) => (
         <ProjectCard
-          key={project.id}
-          id={project.id}
-          name={project.name}
+          key={project.projectId}
+          id={project.projectId}
+          name={project.projectTitle}
           startDate={project.startDate}
           endDate={project.endDate}
         />
