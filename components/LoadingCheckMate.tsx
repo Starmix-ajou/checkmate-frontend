317868d0ef58e06262e84d739ca65d6a483dbb11
pjookim <pjookim@naver.com>
2025-04-27 22:05:52 +0900

    feat: 로딩 컴포넌트의 크기를 조정할 수 있도록 수정 (CM-67)

--- components/LoadingCheckMate.tsx
+++ components/LoadingCheckMate.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
 
-const LoadingCheckMate = () => {
+type LoadingCheckMateProps = {
+  size?: number
+}
+
+const LoadingCheckMate = ({ size = 64 }: LoadingCheckMateProps) => {
+  const squareSize = size / 2
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="cm-loading-wrapper">
-        <div className="cm-square cm-dark"></div>
-        <div className="cm-square cm-light"></div>
+      <div
+        className="cm-loading-wrapper"
+        style={{
+          width: `${size}px`,
+          height: `${size}px`,
+        }}
+      >
+        <div
+          className="cm-square cm-dark"
+          style={{
+            width: `${squareSize}px`,
+            height: `${squareSize}px`,
+          }}
+        ></div>
+        <div
+          className="cm-square cm-light"
+          style={{
+            width: `${squareSize}px`,
+            height: `${squareSize}px`,
+          }}
+        ></div>
       </div>
     </div>
   )
