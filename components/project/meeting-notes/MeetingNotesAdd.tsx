36d01845c27703a0ef76bc5db1c444a8a4d1f336
pjookim <pjookim@naver.com>
2025-05-18 12:20:57 +0900

    fix: 미사용 의존성으로 인한 컴파일 오류를 해결 (CM-53)

--- components/project/meeting-notes/MeetingNotesAdd.tsx
+++ components/project/meeting-notes/MeetingNotesAdd.tsx
@@ -1,9 +1,8 @@
 import { Button } from '@/components/ui/button'
-import { FilePlus } from 'lucide-react'
 
 const MeetingNotesAdd = () => {
   return (
-    <Button variant='cm' className='px-4'>
+    <Button variant="cm" className="px-4">
       회의록 작성
     </Button>
   )
